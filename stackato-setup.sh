04145090cc25567b3bcca1062d4fc9edd961be90
Steven Hsu <stevenhsu0@gmail.com>
2012-09-27 17:11:46 -0700

    Refactored, moved everything to one shell script.

--- stackato-setup.sh
+++ stackato-setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #echo "This scripts does Stackato setup related to filesystem."
+FS=$STACKATO_FILESYSTEM
 if [ -s "$STACKATO_FILESYSTEM"/configuration.php ]
   then
     echo "Configuration file exists. Removing installation folder..."
@@ -7,7 +8,20 @@ if [ -s "$STACKATO_FILESYSTEM"/configuration.php ]
 else
     echo "Configuration file not found. Creating..."
     touch "$STACKATO_FILESYSTEM"/configuration.php
+
+    # create folders in the shared filesystem
+    mkdir -p $FS/images
 fi
-echo "Cleaning up..."
+
+echo "Migrating data to shared filesystem..."
+cp -r images/* $FS/images
+
+echo "Symlink to files in shared filesystem..."
+rm -f configuration.php
 ln -s "$STACKATO_FILESYSTEM"/configuration.php configuration.php
+
+echo "Symlink to folders in shared filesystem..."
+rm -fr images
+ln -s $FS/images images
+
 echo "All Done!"
