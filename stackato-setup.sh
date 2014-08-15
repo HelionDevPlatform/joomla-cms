1ee656b842e9d48b2f2e577dc99b452bc1ea0214
Billy Tat <billyt@activestate.com>
2014-08-15 15:54:54 -0700

    Use non-legacy buildpack

--- stackato-setup.sh
+++ stackato-setup.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 #echo "This scripts does Stackato setup related to filesystem."
 FS=$STACKATO_FILESYSTEM
-if [ -s "$STACKATO_FILESYSTEM"/configuration.php ]
+if [ -s $FS/configuration.php ]
   then
     echo "Configuration file exists. Removing installation folder..."
     rm -rf installation/
 else
     echo "Configuration file not found. Creating..."
-    touch "$STACKATO_FILESYSTEM"/configuration.php
+    touch $FS/configuration.php
 
     # create folders in the shared filesystem
     mkdir -p $FS/images
 fi
 
 echo "Migrating data to shared filesystem..."
-cp -r images/* $FS/images
+cp -r www/images/* $FS/images
 
 echo "Symlink to files in shared filesystem..."
-rm -f configuration.php
-ln -s "$STACKATO_FILESYSTEM"/configuration.php configuration.php
+rm -f www/configuration.php
+ln -s $FS/configuration.php www/configuration.php
 
 echo "Symlink to folders in shared filesystem..."
-rm -fr images
-ln -s $FS/images images
+rm -fr www/images
+ln -s $FS/images www/images
 
 echo "All Done!"
