777e49c4f10bf5e20abc42031affb2e6c11af256
Billy Tat <billyt@activestate.com>
2014-08-29 16:36:19 -0700

    Change to heroku/heroku-buildpack-php

--- stackato-setup.sh
+++ stackato-setup.sh
@@ -14,14 +14,14 @@ else
 fi
 
 echo "Migrating data to shared filesystem..."
-cp -r www/images/* $FS/images
+cp -r images/* $FS/images
 
 echo "Symlink to files in shared filesystem..."
-rm -f www/configuration.php
-ln -s $FS/configuration.php www/configuration.php
+rm -f configuration.php
+ln -s "$FS"/configuration.php configuration.php
 
 echo "Symlink to folders in shared filesystem..."
-rm -fr www/images
-ln -s $FS/images www/images
+rm -fr images
+ln -s $FS/images images
 
 echo "All Done!"
